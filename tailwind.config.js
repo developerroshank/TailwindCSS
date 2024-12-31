2e7d4ac08421f0b1dd33da32d19d223a090007a5
Roshan Kumar <127415063+developerroshank@users.noreply.github.com>
2024-12-31 21:38:50 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  // content: ['./public/**/*/.{html,js, jsx, tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
